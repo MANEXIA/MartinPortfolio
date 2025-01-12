63aa91c664e0d2c629484332b93b58c25a3e8932
Martin Gebiertas <martingebiertas101@gmail.com>
2025-01-12 11:32:20 +0700

    first commit

---
+++ dom.js
@@ -0,0 +1,16 @@
+"use-strict";
+
+// Select all navigation list items
+navItems = document.querySelectorAll('.nav-item');
+
+// Add click event listener to each list item
+navItems.forEach((v, i, arr) => {
+    const btn = v
+    btn.addEventListener('click', () => {
+        // Remove 'active' class from all list items
+        navItems.forEach(nav => nav.classList.remove('active'));
+        
+        // Add 'active' class to the clicked list item
+        btn.classList.add('active');
+    });
+});
