b80089a5860aa6515177b00e66bd2a8f2f36383c
Martin Gebiertas <martingebiertas101@gmail.com>
2026-03-18 06:21:56 +0800

    big update

--- dom.js
+++ dom.js
@@ -1,32 +1,56 @@
 "use-strict";
 
-// Select all navigation list items
-navItems = document.querySelectorAll('.nav-item');
+console.log("sdsdsdsd")
 
-// Add click event listener to each list item
-navItems.forEach((v, i, arr) => {
-    const btn = v
-    btn.addEventListener('click', () => {
-        // Remove 'active' class from all list items
-        navItems.forEach(nav => nav.classList.remove('active'));
-        
-        // Add 'active' class to the clicked list item
-        btn.classList.add('active');
-    });
+let navLinks = document.querySelectorAll('.nav-link')
+
+navLinks.forEach((v, i ,ar) => {
+         v.classList.remove('active')
+
+         v.addEventListener( 'click', () =>{
+            navLinks.forEach(x => x.classList.remove('active'))
+           v.classList.add('active')
+        });
+    
+});   
+
+
+// let wow = (content) =>{
+//    return content + "wow"
+// }
+// alert(wow("sdasdasd"))
+
+let sw = document.querySelector('.switch') 
+let sb = document.querySelector('.switch-btn-light')
+sw.addEventListener('click', () => {
+       sb.classList.toggle('switch-btn-light');
+       sb.classList.toggle('switch-btn-dark');
+       
+})
+
+
+let pc = document.querySelectorAll('.project-card');
+
+pc.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    const title = card.querySelector('.project-title-green');
+    const type = card.querySelector('.project-type-default');
+
+    title.classList.replace('project-title-green', 'project-title-default');
+    type.classList.replace('project-type-default', 'project-type-green');
+  });
+
+   card.addEventListener('mouseleave', () => {
+    const title = card.querySelector('.project-title-default');
+    const type = card.querySelector('.project-type-green');
+
+    title.classList.replace('project-title-default', 'project-title-green');
+    type.classList.replace('project-type-green', 'project-type-default');
+
+   });
+
+
+
 });
-
-
-const herointro = "Hi, I'm Martin Gebiertas"
-
-
-function showNav(){
-    document.getElementById('nav-ul').classList.add('show')
-}
-
-function closeNav(){
-    // alert("adasd")
-    document.getElementById('nav-ul').classList.remove('show')
-}
-
 
 
